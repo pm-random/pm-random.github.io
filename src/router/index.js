e4ab18a6d3a992fa10f624fe058e4c5dea2d9cbe
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-09-10 15:25:01 +0200

    Create 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/:pathMatch(.*)', component: NotFound }
+  ],
 })
 
 export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <img src="logo.png">
+    <p><strong>Page not found :(</strong></p>
+    <p>The requested page could not be found.</p>
+    <router-link to="/">Homepage</router-link>
+  </div>
+</template>
+
+<style>
+  h1 {
+    font-size: 4em;
+  }
+</style>
