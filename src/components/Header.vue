d54b79fc6bd51b55aab67754b2459dcca6bf5b0c
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-09-13 17:56:41 +0200

    Header bar

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <router-link :to="'/'">
+      <img src="/logo.png">
+      <span>PM Random</span>
+    </router-link>
+  </nav>
+</template>
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@600&display=swap');
+
+nav {
+  background-color: #f1f1f1;
+  text-align: left;
+  padding: 8px;
+}
+
+img {
+  height: 40px;
+  vertical-align: middle;
+}
+
+span {
+  font-family: 'Rajdhani', sans-serif;
+  font-size: 2em;
+  vertical-align: middle;
+  padding: 6px;
+}
+
+a {
+  text-decoration: none;
+  color: initial;
+}
+</style>
