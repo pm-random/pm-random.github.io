16cf26c0dd77bcede2300d61a4146cbdce942152
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-09-16 17:47:04 +0200

    Yet another attempt to fix the zoom issue on mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createHead } from '@vueuse/head'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(createHead()).mount('#app')
