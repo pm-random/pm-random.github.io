bee84d8708df00ef9995aae8cb02de053e03fb49
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-10-27 12:30:09 +0200

    Bump game version to 2.14.0

--- src/data.js
+++ src/data.js
@@ -4,4 +4,4 @@ export function JSONFetch(path) {
   return fetch(`${DATA_URL}/json/${path}.json`).then(r => r.json())
 }
 
-export const GAME_VERSION = "2.13.0"
+export const GAME_VERSION = "2.14.0"
