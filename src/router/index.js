319cea4ec6eb793c9b46d70b074cf11f98ac590e
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-10-27 22:09:34 +0200

    router: no need for route names

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Home', component: () => import('../views/Home.vue') },
-    { path: '/characters', name: 'Characters', component: () => import('../views/Characters.vue') },
-    { path: '/monsters', name: 'Monsters', component: () => import('../views/Monsters.vue') },
-    { path: '/theme-skills', name: 'ThemeSkills', component: () => import('../views/ThemeSkills.vue') },
-    { path: '/revenue', name: 'Revenue', component: () => import('../views/Revenue.vue') },
-    { path: '/xp', name: 'XP', component: () => import('../views/XP.vue') },
+    { path: '/', component: () => import('../views/Home.vue') },
+    { path: '/characters', component: () => import('../views/Characters.vue') },
+    { path: '/monsters', component: () => import('../views/Monsters.vue') },
+    { path: '/theme-skills', component: () => import('../views/ThemeSkills.vue') },
+    { path: '/revenue', component: () => import('../views/Revenue.vue') },
+    { path: '/xp', component: () => import('../views/XP.vue') },
     { path: '/:pathMatch(.*)', component: () => import('../views/NotFound.vue') }
   ],
 })
