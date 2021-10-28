159fe5a46cd72eea127ecb85537008f71b1f8a7d
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-10-28 16:08:23 +0200

    Add highcharts-vue to the app

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import { createHead } from '@vueuse/head'
 import VueChartkick from 'vue-chartkick'
 import 'chartkick/highcharts'
+import HighchartsVue from 'highcharts-vue'
 
-createApp(App).use(router).use(createHead()).use(VueChartkick).mount('#app')
+createApp(App).use(router).use(createHead()).use(VueChartkick).use(HighchartsVue).mount('#app')
