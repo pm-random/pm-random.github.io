8bfaf5a57b78b3c02c9f25c117dd207f8790bc3d
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-10-28 20:02:02 +0200

    Remove vue-chartkick package and its dependencies

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createHead } from '@vueuse/head'
-import VueChartkick from 'vue-chartkick'
-import 'chartkick/highcharts'
 import HighchartsVue from 'highcharts-vue'
 
-createApp(App).use(router).use(createHead()).use(VueChartkick).use(HighchartsVue).mount('#app')
+createApp(App).use(router).use(createHead()).use(HighchartsVue).mount('#app')
