0416334f03d858a14fdd80fffcba05b4ed74ff01
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-11-20 15:15:09 +0100

    Timeline (2/?)

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,6 +1,7 @@
 <template>
   <HeaderBar/>
   <h1>Timeline</h1>
+  <highcharts :options="chartOptions"></highcharts>
 </template>
 
 <script>
@@ -17,14 +18,101 @@ export default {
   },
 
   created() {
-    JSONFetch("dev_letter").then(json => {
-
-    });
+    JSONFetch("dev_letter").then(this.letterPrep);
+    JSONFetch("versions").then(this.versionPrep);
+    JSONFetch("announcements").then(this.annoPrep);
   },
 
   data() {
     return {
+      chartOptions: {
+        chart: {
+          zoomType: 'x',
+          type: 'scatter'
+        },
+        credits: { enabled: false },
+        xAxis: {
+          type: 'datetime',
+          visible: true
+        },
+        yAxis: { visible: false },
+        title: null,
+        series: []
+      }
+    }
+  },
 
+  methods: {
+    formatDate(isoDate) {
+      let [year, month, day] = isoDate.split('-');
+      return `${day}/${month}/${year}`;
+    },
+
+    letterPrep(json) {
+      const base = {
+        name: "Dev Letter",
+        color: "green",
+        marker: { symbol: 'circle' },
+        tooltip: {
+          pointFormat: '<b>#{point.number}</b> - {point.date}'
+        }
+      }
+
+      base.data = json.letters.map((value, index) =>
+        ({
+          x: Date.parse(value.date),
+          y: 3,
+          number: index + 1,
+          date: this.formatDate(value.date)
+        })
+      );
+
+      this.chartOptions.series.push(base);
+    },
+
+
+    versionPrep(json) {
+      const base = {
+        name: "Version",
+        color: "red",
+        marker: { symbol: 'circle' },
+        tooltip: {
+          pointFormat: '<b>{point.name}</b> - {point.date}'
+        }
+      }
+
+      base.data = json.map((value, index) =>
+        ({
+          x: Date.parse(value.date),
+          y: 2,
+          name: value.name,
+          date: this.formatDate(value.date)
+        })
+      );
+
+      this.chartOptions.series.push(base);
+    },
+
+    annoPrep(json) {
+      const base = {
+        name: "Major Announcement",
+        color: "blue",
+        marker: { symbol: 'diamond' },
+        tooltip: {
+          pointFormat: '<b>{point.name}</b> - {point.date}'
+        }
+      }
+
+      base.data = json.map((value, index) =>
+        ({
+          x: Date.parse(value.date),
+          y: 1,
+          name: value.name,
+          date: this.formatDate(value.date)
+        })
+      );
+
+      this.chartOptions.series.push(base);
     }
   }
 }
