d1199d6e0b2e8c8b6631e56b63cc4dfe2fb25b8c
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2021-12-18 04:54:35 +0100

    Smaller border radius on tables

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ table {
   margin: auto;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   border-bottom: 3px solid #aaf;
-  border-radius: 18px 18px 0px 0px;
+  border-radius: 12px 12px 0px 0px;
   overflow: hidden;
 }
 
