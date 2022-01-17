8b0665babccb390654ae23beae50bd8d14553f8c
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2022-01-17 22:11:39 +0100

    TopBar: responsive collapse and links to main website areas

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,13 +1,27 @@
 <template>
-  <nav class="navbar navbar-light">
+  <nav class="navbar navbar-light navbar-expand-md">
     <div class="container-fluid">
-      <router-link :to="'/'" id="branding">
+      <router-link :to="'/'" class="navbar-brand">
         <img src="/favicon.ico">
         <NameAndVersion/>
       </router-link>
-      <a href="https://github.com/pm-random">
-        <button class="btn btn-outline-primary" href="https://github.com/pm-random">View on Github</button>
-      </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav me-auto">
+          <router-link :to="'/characters'" class="nav-link">Characters</router-link>
+          <router-link :to="'/monsters'" class="nav-link">Pokémon</router-link>
+          <router-link :to="'/revenue'" class="nav-link">Revenue</router-link>
+          <router-link :to="'/voice-actors'" class="nav-link">VA</router-link>
+        </div>
+
+        <a href="https://github.com/pm-random">
+          <button class="btn btn-outline-primary" href="https://github.com/pm-random">View on Github</button>
+        </a>
+      </div>
     </div>
   </nav>
 </template>
@@ -28,14 +42,13 @@ export default {
 
 
 <style scoped>
-#branding {
+.navbar-brand {
   display: inline-flex;
   gap: 6px;
 }
 
 nav {
   background-color: #f1f1f1;
-  padding: 8px;
   box-shadow: 0px 1px 3px #BBBBBB;
 }
 
