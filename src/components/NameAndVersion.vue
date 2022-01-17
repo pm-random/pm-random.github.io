5fcb62f22de672ad08bf04b0d90ec900b4d670f8
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2022-01-17 17:36:14 +0100

    NameAndVersion component

---
+++ src/components/NameAndVersion.vue
@@ -0,0 +1,53 @@
+<template>
+  <span id="all" :style="style">
+    <span id="name">PM Random</span>
+    <sup id="version">{{ version }}</sup>
+  </span>
+</template>
+
+
+<script>
+import { JSONFetch } from '@/data'
+
+export default {
+  props: {
+    home: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    style() {
+      return {
+        "--name-size": `${this.home ? 48 : 28}px`,
+        "--version-size": `${this.home ? 36 : 20}px`
+      }
+    }
+  },
+  created() {
+    JSONFetch("version").then(json => this.version = json);
+  },
+  data() {
+    return {
+      version: null
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+#all {
+  font-family: 'Rajdhani', sans-serif;
+}
+
+#name {
+  color: black;
+  font-size: var(--name-size);
+}
+
+#version {
+  color: var(--accent-color);
+  font-size: var(--version-size);
+}
+</style>
