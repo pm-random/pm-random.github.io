7e67cb5bd5b8984d378937e675a53e9d6564df1b
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2022-01-18 06:00:13 +0100

    App.vue: Delete dead css

--- src/App.vue
+++ src/App.vue
@@ -36,35 +36,4 @@ tbody tr:nth-child(odd) {
 tbody tr:nth-child(even) {
   background-color: #f9f9f9;
 }
-
-/*
-.responsive-x {
-  overflow-x: auto;
-}
-
-
-table {
-  border-collapse: collapse;
-  margin: auto;
-  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
-  border-bottom: 3px solid #aaf;
-  border-radius: 12px 12px 0px 0px;
-  overflow: hidden;
-}
-
-th {
-  padding: 10px;
-  background-color: #ccf;
-  color: black;
-}
-
-tr td {
-  padding: 5px;
-  border: 1px lightgray solid;
-}
-
-td img {
-  vertical-align: middle;
-}
-*/
 </style>
