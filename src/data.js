ce34b0ea04c29b22c8a2afdb152dbcd855057132
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2022-02-21 12:57:05 +0100

    JSONFetch: Always check if json was updated

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 export const DATA_URL = "https://pm-random.github.io/data"
 
 export function JSONFetch(path) {
-  return fetch(`${DATA_URL}/json/${path}.json`).then(r => r.json())
+  return fetch(`${DATA_URL}/json/${path}.json`, {cache: "no-cache"}).then(r => r.json())
 }
