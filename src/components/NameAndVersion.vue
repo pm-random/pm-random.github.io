eb4c8762c3ed7f7dedc7b820c8072b610d25b518
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2022-06-27 02:45:05 +0200

    Move version code under name of the site

--- src/components/NameAndVersion.vue
+++ src/components/NameAndVersion.vue
@@ -1,8 +1,8 @@
 <template>
-  <span id="all">
-    <span id="name" :style="nameStyle">PM Random</span>
-    <sup id="version" :style="versionStyle">{{ version }}</sup>
-  </span>
+  <div id="all">
+    <div id="name" :style="nameStyle">PM Random</div>
+    <div id="version" :style="versionStyle">{{ version }}</div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -14,7 +14,7 @@ const props = defineProps({
 });
 
 const nameStyle = computed(() => `font-size: ${props.home ? "48" : "24"}px`);
-const versionStyle = computed(() => `font-size: ${props.home ? "36" : "18"}px`);
+const versionStyle = computed(() => `font-size: ${props.home ? "28" : "14"}px`);
 
 const version = ref();
 JSONFetch("version").then(json => (version.value = json));
@@ -23,13 +23,18 @@ JSONFetch("version").then(json => (version.value = json));
 <style scoped>
 #all {
   font-family: "Rajdhani", sans-serif;
+  width: min-content;
+  margin: auto;
 }
 
 #name {
   color: black;
+  white-space: nowrap;
 }
 
 #version {
   color: var(--accent-color);
+  text-align: right;
+  margin-top: -10%;
 }
 </style>
