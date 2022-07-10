637890f08171959b5097344da2d56953387a22ab
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2022-07-10 11:10:21 +0200

    Fix hinted character table on mobile devices

--- src/components/HintedCharacterTable.vue
+++ src/components/HintedCharacterTable.vue
@@ -1,39 +1,41 @@
 <template>
-  <div class="table-responsive">
+  <div>
     <div class="d-flex justify-content-between align-items-center m-2">
       <h4 v-html="category.title" class="m-0"></h4>
       <span class="badge bg-primary rounded-pill">{{ category.characters.length }}</span>
     </div>
     <div class="border border-1"></div>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th v-if="category.hasImages">Images</th>
-          <th v-if="category.hasNotes">Notes</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="character in category.characters" :key="character.name">
-          <td>{{ character.name }}</td>
-          <td v-if="category.hasImages">
-            <span v-if="character.images === undefined" class="dash">
-              &mdash;
-            </span>
-            <a v-for="image in character.images" :href="image" :key="image">
-              <img class="image img-thumbnail mx-1" :src="image" />
-            </a>
-          </td>
-          <template v-if="category.hasNotes">
-            <td v-if="character.notes === undefined">&mdash;</td>
-            <td v-else v-html="character.notes.join('<br>')"></td>
-          </template>
-          <td v-if="character.days === 1">{{ character.prettyDate() }} ({{ character.days }} day)</td>
-          <td v-else>{{ character.prettyDate() }} ({{ character.days }} days)</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th v-if="category.hasImages">Images</th>
+            <th v-if="category.hasNotes">Notes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="character in category.characters" :key="character.name">
+            <td>{{ character.name }}</td>
+            <td v-if="category.hasImages">
+              <span v-if="character.images === undefined" class="dash">
+                &mdash;
+              </span>
+              <a v-for="image in character.images" :href="image" :key="image">
+                <img class="image img-thumbnail mx-1" :src="image" />
+              </a>
+            </td>
+            <template v-if="category.hasNotes">
+              <td v-if="character.notes === undefined">&mdash;</td>
+              <td v-else v-html="character.notes.join('<br>')"></td>
+            </template>
+            <td v-if="character.days === 1">{{ character.prettyDate() }} ({{ character.days }} day)</td>
+            <td v-else>{{ character.prettyDate() }} ({{ character.days }} days)</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
