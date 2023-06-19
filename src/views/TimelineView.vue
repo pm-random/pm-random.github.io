a920a165e459bd5837e1da90620efd8030822914
MiniEmerald <41841579+MiniEmerald@users.noreply.github.com>
2023-06-19 02:46:35 +0200

    new timeline

--- src/views/TimelineView.vue
+++ src/views/TimelineView.vue
@@ -1,115 +1,104 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useHead } from "@unhead/vue";
 import { fetch_cdn_data } from "@/cdn";
 import { prettyDate } from "@/utils";
 
-interface Version {
-  name: string;
-  date: string;
-  highlight?: boolean;
+interface Item {
+  name: string
+  date: string
+  category?: string
+  link?: string
+  detail?: string
+  weight?: number
 }
 
-interface Letter {
-  link: string;
-  date: string;
+function groupBy<K, V>(list: Array<V>, keyGetter: ((item: V) => K)) {
+  const map = new Map<K, Array<V>>();
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
 }
 
-interface Milestone {
-  name: string;
-  date: string;
-  link: string;
+
+function itemCmp(a: Item, b: Item) {
+  const dateCmpResult = b.date.localeCompare(a.date);
+  if (dateCmpResult !== 0)
+    return dateCmpResult;
+  if (a.weight !== undefined && b.weight === undefined)
+    return -1;
+  if (a.weight === undefined && b.weight !== undefined)
+    return 1;
+  if (a.weight === undefined && b.weight === undefined)
+    return a.name.localeCompare(b.name);
+
+  const weightCmpResult = b.weight!! - a.weight!!;
+  if (weightCmpResult !== 0)
+    return weightCmpResult;
+
+  return a.name.localeCompare(b.name);
 }
 
-interface Dated {
-  date: string;
-}
-
-interface Timeline {
-  versions: Array<Version>;
-  letters: Array<Letter>;
-  milestones: Array<Milestone>;
-}
-
-function datedCmp(a: Dated, b: Dated) {
-  if (a.date < b.date) return 1;
-  if (b.date < a.date) return -1;
-  return 0;
-}
-
-const versions = ref<Array<Version>>();
-const letters = ref<Array<Letter>>();
-const milestones = ref<Array<Milestone>>();
 
 useHead({ title: "Timeline | PM Random" });
 
-fetch_cdn_data<Timeline>("timeline").then((timeline) => {
-  letters.value = timeline.letters.sort(datedCmp);
-  milestones.value = timeline.milestones.sort(datedCmp);
-  versions.value = timeline.versions.sort(datedCmp);
+const weightMap = new Map([[1, "timeline--minor"], [2, "timeline--major"]]);
+
+const items = ref<Array<Item>>();
+const comps = computed(() => {
+  if (items.value === undefined)
+    return undefined;
+  else
+    return groupBy<string, Item>(items.value, item => item.date);
+});
+
+
+fetch_cdn_data<Array<Item>>("timeline").then(timeline => {
+  items.value = timeline.sort(itemCmp);
 });
 </script>
 
 <template>
   <h1>Timeline</h1>
-  <template v-if="letters != null">
-    <h2>Dev Letters</h2>
+  <template v-if="comps != null">
     <div class="card">
       <table class="w-100">
         <thead>
           <tr>
-            <th>Name</th>
             <th>Date</th>
+            <th>Events</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(letter, index) in letters" :key="letter.link">
+          <tr v-for="[date, items] in comps" :key="date">
+            <td>{{ prettyDate(date) }}</td>
             <td>
-              <a :href="letter.link">Dev Letter #{{ letters.length - index }}</a>
+              <div v-for="(item, index) in items" :key="index">
+                <span :class="item.weight ? weightMap.get(item.weight) : null">
+                  <a v-if="item.link" :href="item.link">{{ item.name }}</a>
+                  <template v-else>{{ item.name }}</template>
+                </span>
+              </div>
             </td>
-            <td>{{ prettyDate(letter.date) }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-    <h2>Milestones</h2>
-    <div class="card">
-      <table class="w-100">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="milestone in milestones" :key="milestone.name">
-            <td>
-              <a v-if="milestone.link" :href="milestone.link">{{ milestone.name }}</a>
-              <template v-else>{{ milestone.name }}</template>
-            </td>
-            <td>{{ prettyDate(milestone.date) }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-    <h2>Versions</h2>
-    <div class="card">
-      <table class="w-100">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="version in versions" :key="version.name">
-            <td>{{ version.name }}</td>
-            <td>{{ prettyDate(version.date) }}</td>
           </tr>
         </tbody>
       </table>
     </div>
   </template>
 </template>
+
+<style scoped>
+.timeline--major {
+  font-weight: 700;
+}
+.timeline--minor {
+  font-weight: 600;
+}
+</style>
